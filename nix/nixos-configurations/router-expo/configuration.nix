bb1c53f84e4216d0c6dfd214b3c4ec53f474693f
Robert James Hernandez <rob@sarcasticadmin.com>
2026-02-01 01:59:16 +0000

    nixosConfigurations.router-expo: enable alloy

--- nix/nixos-configurations/router-expo/configuration.nix
+++ nix/nixos-configurations/router-expo/configuration.nix
@@ -10,7 +10,10 @@
       frrConferenceInterface = "fiber1";
     };
     services.ssh.enable = true;
-
+    services.alloy = {
+      enable = true;
+      remoteWrite = "10.128.3.20";
+    };
     users.conjones.enable = true;
     users.djacu.enable = true;
     users.kylerisse.enable = true;
